73e89dc7455d38d6b4b1873c88d5ebc5fddef501
jhf005 <78598576+jhf005@users.noreply.github.com>
2026-02-28 10:07:52 +0800

    Refactor 2.clone.sh to streamline cloning process
    Removed unnecessary cloning of Lean source code and updated comments.

--- preset-lede/2.clone.sh
+++ preset-lede/2.clone.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# 拉取 Lean 源码（如果已有则跳过）
-[ -d "lede" ] || git clone https://github.com/coolsnowwolf/lede.git
-
-# 进入源码目录
-cd lede
-
-# 添加插件源
+# 添加 EasyTier 插件源
 git clone https://github.com/zhsj/easytier-openwrt.git package/easytier
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# 更新并安装 feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 添加 AdGuardHome 插件源
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
