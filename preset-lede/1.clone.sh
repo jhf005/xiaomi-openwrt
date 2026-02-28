d2efc04f60717765c4e5e3c9c6e20a01c2ac04ee
jhf005 <78598576+jhf005@users.noreply.github.com>
2026-02-28 10:10:11 +0800

    Add EasyTier and AdGuardHome plugin sources

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -12,3 +12,8 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
 git clone --depth 1 https://github.com/kenzok8/small.git
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
+# 添加 EasyTier 插件源
+git clone https://github.com/zhsj/easytier-openwrt.git package/easytier
+
+# 添加 AdGuardHome 插件源
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
